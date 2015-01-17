d672577c44f7114e5b074883b28ecc9114115ba2
Kenneth Rhee <kenneth.h.rhee@gmail.com>
2015-01-17 15:39:11 -0500

    stripe

--- stripe.py
+++ stripe.py
@@ -1,12 +1,35 @@
-def charge(transaction_id):
-	pass
+CREATED = 0
+CHARGED = 1
+
+charges = db.charges
+
+def charge(_id):
+	update = {}
+
+	#this would be where we charge the card if it worked
+	#imagine some code
+
+	update['status'] = CHARGED
+
+	charges.update({'_id' : _id}, {'$set', update})
+
+	return True
 
 def create_charge(amount, buyer_id, seller_email, cl_id):
-	charges = db.charges
 	charge = {}
+	charge['_id'] = uuid.uuid4()
 	charge['amount'] = amount
 	charge['buyer_id'] = buyer_id
+	charge['seller_email'] = seller_email
+	charge['cl_id'] = cl_id
+	charge['status'] = CREATED
 	
+	charges.insert(charge)
 
-def set_charge_amount(transaction_id, amount):
-	pass
+	return True
+
+def set_charge_amount(_id, amount):
+	update = {'amount' : amount}
+	charges.update({'_id' : _id}, {'$set', update})
+
+	return True
