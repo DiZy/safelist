9137ac9770f7f48459592d265cca8cdc4bc52074
Kenneth Rhee <kenneth.h.rhee@gmail.com>
2015-01-17 15:27:50 -0500

    unique id for users

--- stripe.py
+++ stripe.py
@@ -1,8 +1,12 @@
 def charge(transaction_id):
 	pass
 
-def create_charge(amount, buyer_id, seller_id, cl_id):
-	pass
+def create_charge(amount, buyer_id, seller_email, cl_id):
+	charges = db.charges
+	charge = {}
+	charge['amount'] = amount
+	charge['buyer_id'] = buyer_id
+	
 
-def change_charge_amount(amount, cl_id):
+def set_charge_amount(transaction_id, amount):
 	pass
